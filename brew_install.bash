b18d907578ce2eb4aee5938ef9c05a19661e4c93
Jim Larkin <jimlarkin@Jims-MacBook-Pro.local>
2020-09-12 18:28:07 -0400

    Added brew_install

---
+++ brew_install.bash
@@ -0,0 +1,13 @@
+
+# install HomeBrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+#cask installs
+brew cask install visual-studio-code
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install microsoft-office
+
+#install non-casks
+brew install python@3.8
+brew install git
