0c747b1f79a143b21930b9e43c25e7a11690b116
Jim Larkin <jimlarkin@Jims-MacBook-Pro.local>
2020-09-12 18:37:05 -0400

    added source code pro

--- brew_install.bash
+++ brew_install.bash
@@ -2,6 +2,9 @@
 # install HomeBrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+#install  source code pro 
+brew tap homebrew/cask-fonts && brew cask install font-source-code-pro
+
 #cask installs
 brew cask install visual-studio-code
 brew cask install dropbox
