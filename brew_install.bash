9d32b7fa833a406b6218ddbb1acfdc9978be4942
Jim Larkin <jjlarkin@gmail.com>
2021-11-05 13:50:49 -0400

    Update brew_install.bash
    Added tree commend tool to the list

--- brew_install.bash
+++ brew_install.bash
@@ -2,12 +2,13 @@
 # install HomeBrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-#install  source code pro 
+#install source code pro font
 brew tap homebrew/cask-fonts && brew cask install font-source-code-pro
 
 #install non-casks
 brew install python@3.8
 brew install git
+brew install tree
 
 #cask installs
 brew cask install visual-studio-code
